7d20f74f7940053216fe248d3836ae7002653ba9
M.j Kim <115759222+Rachelpoundcakes@users.noreply.github.com>
2023-07-15 03:56:33 +0900

    Update and rename Cats and dogs/cat_dog_customdataset_2.py to customdataset.py

--- customdataset.py
+++ customdataset.py
@@ -1,14 +1,14 @@
 """
-dataset 
+dataset
     - train
-        - cat 
+        - cat
             - cat1.jpg....
         - dog
-    - val  
-        - cat 
+    - val
+        - cat
         - dog
     - test
-        - cat 
+        - cat
         - dog
 """
 from torch.utils.data.dataset import Dataset
@@ -16,12 +16,11 @@ import os
 import glob
 from PIL import Image
 
-label_dit = {"cat": 0, "dog": 1}
+label_dic = {"cat": 0, "dog": 1}
 
 
 class cat_dog_mycustomdataset(Dataset):
     def __init__(self, data_path):
-        # csv folder 읽기, 변환 할당, 데이터 필터링 등 과 같은 초기 논리가 발생
         self.all_data_path = glob.glob(os.path.join(data_path, '*', '*.jpg'))
 
     def __getitem__(self, index):
@@ -29,7 +28,7 @@ class cat_dog_mycustomdataset(Dataset):
         img = Image.open(image_path).convert("RGB")
         label_temp = image_path.split("/")
         # ['.', 'dataset', 'train', 'cat', 'cat.2718.jpg']
-        label = label_dit[label_temp[3]]
+        label = label_dic[label_temp[3]]
 
         return img, label
 
@@ -38,7 +37,7 @@ class cat_dog_mycustomdataset(Dataset):
         return len(self.all_data_path)
 
 
-test = cat_dog_mycustomdataset("./dataset/train/")
+# test = cat_dog_mycustomdataset("./dataset/train/")
 
-for i in test:
-    pass
+# for i in test:
+#    pass
